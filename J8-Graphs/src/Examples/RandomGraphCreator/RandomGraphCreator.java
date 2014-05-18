e50b460e2c4d5cd69419ac9c452422481593d936
Normo <normo157@gmail.com>
2014-05-18 19:11:14 +0200

    Anpassung an neue RandomGraph-Methoden

--- J8-Graphs/src/Examples/RandomGraphCreator/RandomGraphCreator.java
+++ J8-Graphs/src/Examples/RandomGraphCreator/RandomGraphCreator.java
@@ -1,5 +1,8 @@
 package Examples.RandomGraphCreator;
 
+import java.util.Date;
+
+import J8Graphs.model.DiGraph;
 import J8Graphs.util.GraphWriter;
 import J8Graphs.util.RandomDiGraph;
 
@@ -22,14 +25,27 @@ public class RandomGraphCreator {
 			}
 			
 			GraphWriter gw = new GraphWriter(path);
+			DiGraph g;
+			long start, runningTime;
 			
 			if (args.length > 3 && args[3].equals("azyklisch")) {
 				System.out.println("Erzeuge zufälligen azyklischen gerichteten Graphen mit " + nodeAmount + " Knoten und " + edgeAmount + " Kanten.");
-				gw.writeGraph(RandomDiGraph.getAcyclicRandomDiGraph(nodeAmount, edgeAmount));
+				start = new Date().getTime();
+				g = RandomDiGraph.getCoordinatedRandomDiGraph(nodeAmount, edgeAmount, true);
+				runningTime = new Date().getTime() - start;
+				System.out.println("Laufzeit Graph erzeugen: " + runningTime + " ms");
 			} else {
 				System.out.println("Erzeuge zufälligen gerichteten Graphen mit " + nodeAmount + " Knoten und " + edgeAmount + " Kanten.");
-				gw.writeGraph(RandomDiGraph.getRandomDiGraph(nodeAmount, edgeAmount));
+				start = new Date().getTime();
+				g = RandomDiGraph.getCoordinatedRandomDiGraph(nodeAmount, edgeAmount, false);
+				runningTime = new Date().getTime() - start;
+				System.out.println("Laufzeit Graph erzeugen: " + runningTime + " ms");
 			}
+			
+			long start2 = new Date().getTime();
+			gw.writeGraph(g);
+			long runningTime2 = new Date().getTime() - start2;
+			System.out.println("Laufzeit Graph schreiben: " + runningTime2 + " ms");
 			
 			System.out.println("Ergebnis in Outputdatei: " + path);
 			
